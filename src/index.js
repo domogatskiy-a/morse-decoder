be3d50dd007a6c5a5ff4507a2ccca206e599b38d
Домогатский Алексей <domogatskiy.a@yandex.ru>
2023-01-26 18:10:59 +0300

    feat: variant 1

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letter = '';
+    return expr
+        .split('')
+        .map((a, i, arr) => i % 2 !== 0? false : a + arr[i+1])
+        .filter(a => a)
+        .map(a => {
+            switch (a) {
+                case '00': return false;
+                case '01': return false;
+                case '10': return '.';
+                case '11': return '-';
+                case '**': return ' ';
+            }
+        })
+        .map((a, i) => {
+            if (!(i % 5)){
+                letter = '';
+            };
+            letter += a ? a : ''; 
+            if (!((i + 1) % 5)){
+                return letter == '     ' ? ' ' : MORSE_TABLE[letter];
+            }
+        })
+        .filter(a => a)
+        .join('');
+        
+    //return expr;
 }
 
 module.exports = {
     decode
 }
+
+//console.log(module.exports.decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
